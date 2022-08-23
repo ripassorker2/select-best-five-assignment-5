675bcad509ccf2fe3087b8e1721a6534df5ca81d
Ripassorker <ripassorker02@glail.com>
2022-08-23 15:22:09 +0600

    all work done

--- script.js
+++ script.js
@@ -9,25 +9,17 @@ let playersNameArray = [];
 function display(playersName) {
     let orderList = document.getElementById('players-list');
     orderList.innerText = '';
-
-    if (playersName.length <= 5) {
-        for (let i = 0; i < playersName.length; i++) {
-            let name = playersNameArray[i];
-            let li = document.createElement('li');
-            li.innerText = name;
-            orderList.appendChild(li);
-        }
+    for (let i = 0; i < playersName.length; i++) {
+        let name = playersNameArray[i];
+        let li = document.createElement('li');
+        li.innerText = name;
+        orderList.appendChild(li);
     }
-    else {
-        alert('You cannot  selected more then five players !!');
-        return;
-    }
-
 }
 //  ----------disabled button function-----------
 
 function disabled(names) {
-    names.style.backgroundColor = 'gray';
+    names.style.backgroundColor = 'lightgray';
     names.disabled = true;
     return;
 }
@@ -38,7 +30,15 @@ function playersName(names) {
 
     let players = names.parentNode.children[0].innerText;
     let player = [players];
-    playersNameArray.push(player);
+
+    if (playersNameArray.length < 5) {
+        playersNameArray.push(player);
+    }
+    else {
+        alert('You cannot  selected more then five players !!  So please refresh this page !!');
+        return;
+    }
+
 
     display(playersNameArray);
     document.getElementById('select-number').innerText = playersNameArray.length;
@@ -59,10 +59,7 @@ function inputAmoutFeild(id) {
 
 function perPlayerTotalAmount() {
     let perPlayer = inputAmoutFeild('per-player-feild');
-    if (playersNameArray.length > 5) {
-        playersNameArray.length = 5
-        return;
-    }
+
     let totalAmount = playersNameArray.length * perPlayer;
     return totalAmount;
 
